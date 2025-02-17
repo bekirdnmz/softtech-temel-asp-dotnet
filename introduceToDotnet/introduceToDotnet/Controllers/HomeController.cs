61b6b517b60bac0250eb680c78095b7d83f4b203
türkay ürkmez <turkay.urkmez@outlook.com>
2025-02-17 11:33:03 +0300

    first commit :)

---
+++ introduceToDotnet/introduceToDotnet/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace introduceToDotnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //HttpResponse olarak dönebilecekler:
+            //1. HTML, CSS, JS
+            //2. JSON
+            //3. XML
+            //4. File
+
+
+            //return "Şu anda Home/Index sayfasındasınız" ;
+            return View();
+        }
+    }
+}
---
+++ introduceToDotnet/introduceToDotnet/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
+
+
+
+var app = builder.Build();
+//gelen http isteğini, rotaya yönlendir:
+
+app.UseRouting();
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
